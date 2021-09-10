29c77666649872d3ecc50bfa1237219f66316cf0
FERDdeveloper <86313430+FERDdeveloper@users.noreply.github.com>
2021-09-10 13:01:44 +0200

    bluh :)
    you kinda smell like a baka :D

---
+++ PhobosC/net/V390n1ck/a/b/c/locked/WALTEM/malw/.jat executer/jatexec.java
@@ -0,0 +1,37 @@
+package PhobosC.net.V390n1ck.a.b.c.locked.WALTEM.malw;
+
+import me.H03_sullinkq.WALTEM.malw._jat_executer._jat;
+
+@mod.register(name = "jat executer", author = "ferderplays")
+public class J {
+    int i = boolean 1;
+    boolean 1 {
+        var1 = throwable.True;
+        var2 = throwable.True;
+        var3 = throwable.True;
+        var4 = throwable.True;
+        var5 = vars;
+    }
+    boolean 2 {
+        if(detectedFile = bugfixerr.jat) {
+            do {
+                execute.file(file = bugfixer.jat) as (Java.lang.executeJavaFile)
+            }
+            while(file.executed(file = bugfixer.jat)) {
+                software.createNewSoftware {
+                    software.name = "bug fixer";
+                    software.version = "0.0.1"
+                    software.author = "ferderplays, V390n1ck"
+    int s
+    {
+                        void(int[s], args) {
+                            var1.getArgs();
+                            var2.getArgs();
+                            var3.getArgs();
+                            var4.getArgs();
+                            var5.getArgs();
+                            args.add(visual = True);
+                            args.add(name = nihao);
+                        }
+                    }
+}}}}}
