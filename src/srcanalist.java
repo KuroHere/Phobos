ba84bd3bc7a6e1cd260b00eb3c0322f93ae85122
FERDdeveloper <86313430+FERDdeveloper@users.noreply.github.com>
2021-09-10 15:35:37 +0200

    bluh :)

---
+++ src/srcanalist.java
@@ -0,0 +1,5 @@
+src.getInfo(src = src.Client.PhobosC());
+info = this.developers = "ferderplays"
+       this.description = "made by ferderplays.jar#0288 with help of veruch#1774"
+       this.developersDiscord = ferderplays = "ferderplays.jar#0288";
+                                v390n1ck =    "veruch#1774";
